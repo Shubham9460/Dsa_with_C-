f18d0e6e4ae25f11922d7ba64e260d0c6178414b
Shubham parmar <sp2738723@gmail.com>
2025-07-04 16:01:42 +0530

    kadanes algoritham

--- Array/Max_subarray_sum.cpp
+++ Array/Max_subarray_sum.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int subarray(int *arr,int n){
+int maxsubarraysum1(int *arr,int n){
     int max_sum = INT16_MIN;
     for(int start= 0 ; start <n; start++){
         for(int end=start; end<n ; end++){
@@ -14,10 +14,38 @@ int subarray(int *arr,int n){
     }
     cout<<max_sum;
 }
+int maxsubarraysum2(int *arr,int n){
+    int max_sum = INT16_MIN;
+    for(int start= 0 ; start <n; start++){//2
+         int currunt_sum=0;
+        for(int end=start; end<n ; end++){//3,4,5,6
+           currunt_sum += arr[end];
+            
+            max_sum=max(max_sum,currunt_sum);
+        }
+    }
+    cout<<max_sum;
+}
+
+//KADANES ALGORITHAM (MOST IMPORTENT)
+int maxsubarraysum3(int *arr, int n){
+    int max_sum= INT16_MIN;
+    int currunt_sum= 0;
+
+    for(int i=0;i<n;i++){
+        currunt_sum +=arr[i];
+        max_sum = max(currunt_sum,max_sum);
+
+        if(currunt_sum<0){
+            currunt_sum=0;
+        }
+    }
+    cout<<max_sum;
+}
 
 int main(){
-    int arr[6]={2,-3,6,-4,4,8};
-    int n=6;
+    int arr[7]={2,-3,6,-4,4,8,3};
+    int n=7;
 
-subarray(arr,n);
+maxsubarraysum3(arr,n);
 }
